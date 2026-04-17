df0fab086376a65193320e0b067bab77b90465fb
odstx <hi@crtrpt.com>
2026-04-17 23:42:36 +0800

    feat: add enable_registration config to control user registration
    - Add enable_registration config option (default: true)
    - Check enableRegistration in Register handler
    - Return 403 when registration is disabled

--- backend/controllers/controllers.go
+++ backend/controllers/controllers.go
@@ -14,7 +14,7 @@ import (
 )
 
 func RegisterRoutes(router *gin.Engine, logger *zap.Logger, userService *services.UserService, oidcService *services.OIDCService, appService *services.AppService, agentService *services.AgentService, rateLimiter *middleware.RateLimiter, cfg *config.Config, db *gorm.DB) {
-	userController := NewUserController(logger, userService, oidcService)
+	userController := NewUserController(logger, userService, oidcService, cfg.EnableRegistration)
 	systemController := NewSystemController(logger, cfg)
 	appController := NewAppController(logger, appService)
 	agentController := NewAgentController(logger, agentService)
